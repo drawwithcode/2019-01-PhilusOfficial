ff83bca121f6917689e5017820ddf57c9896918d
PhilusOfficial <luciricofedefora@gmail.com>
2019-09-18 12:32:43 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,26 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+
+  frameRate(60);
+  background(100);
 }
 
 function draw() {
-  // put drawing code here
+  translate(width/2, height/2);
+  rotate(frameCount*3);
+  noFill();
+
+  stroke(
+    lerpColor(
+    color("aquamarine"),
+    color(255, 0, 68),
+    frameCount/120
+    )
+  );
+  ellipse(100, 0, 250);
+
+  if(frameCount==120) {noLoop()};
 }
